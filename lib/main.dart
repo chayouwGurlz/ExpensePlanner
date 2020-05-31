af60fa982c0aa19f2812719ca18d05a98f9331dc
Irine Octaviana <chayouw_gurlz@yahoo.com>
2020-05-31 19:16:42 +0700

    - WIP: split into model & widget folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import './transaction.dart';
+import './widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,20 +14,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
 
   /*String titleInput;
   String amountInput;*/
@@ -85,63 +70,14 @@ class MyHomePage extends StatelessWidget {
                       print(titleController.text);
                       print(amountController.text);
                     },
-                  )
+                  ),
                 ],
               ),
-            )
+            ),
           ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container( //only container has a boxDecoration, column hasn't
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${tx.amount}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                              tx.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            //DateFormat('yyyy/MM/dd').format(tx.date),
-                            DateFormat.yMMMd().format(tx.date),
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-              );
-            }).toList(),
-          ),
-        ]
-      ), 
+          TransactionList()
+        ],
+      ),
     );
   }
 }
