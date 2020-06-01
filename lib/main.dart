0ce0b7b52c78aef1ceb64c84f2006acf0b2a66f1
Irine Octaviana <chayouw_gurlz@yahoo.com>
2020-06-01 08:33:40 +0700

    - WIP: split into new & user transaction file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import './widgets/transaction_list.dart';
+import './widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -17,8 +17,6 @@ class MyHomePage extends StatelessWidget {
 
   /*String titleInput;
   String amountInput;*/
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -38,44 +36,7 @@ class MyHomePage extends StatelessWidget {
             ),
             elevation: 5,
           ),
-          Card(
-            elevation: 5,
-            child: Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                    ),
-                    controller: titleController,
-                    /*onChanged: (value){
-                      titleInput = value;
-                    },*/
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Amount',
-                    ),
-                    controller: amountController,
-                    //onChanged: (value) => amountInput = value,
-                  ),
-                  FlatButton(
-                    child: Text('Add Transaction'),
-                    textColor: Colors.purple,
-                    onPressed: (){
-                      /*print(titleInput);
-                      print(amountInput);*/
-                      print(titleController.text);
-                      print(amountController.text);
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          TransactionList()
+          UserTransactions(),
         ],
       ),
     );
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -2,31 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
-class TransactionList extends StatefulWidget {
-  @override
-  _TransactionListState createState() => _TransactionListState();
-}
-
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: _userTransactions.map((tx) {
+      children: transactions.map((tx) {
         return Card(
           child: Row(
             children: <Widget>[
