83c5442bddb3098472d92c34b5fae00fa39b0e26
Irine Octaviana <chayouw_gurlz@yahoo.com>
2020-06-01 19:45:48 +0700

    - set list with listview.builder (automatically activate scrollview, and reduce memory usage)
    - give restriction only 2 decimal places, and doesn't add list if the title is empty or the amount is less than 0
    - set soft keyboard only for number if amount input is enabled
    - centralize submitData function that will be called more than one

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -7,6 +7,18 @@ class NewTransaction extends StatelessWidget {
 
   NewTransaction(this.addTx);
 
+  void submitData(){
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0){
+      return;
+    }
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -21,6 +33,7 @@ class NewTransaction extends StatelessWidget {
                 labelText: 'Title',
               ),
               controller: titleController,
+              onSubmitted: (_) => submitData,
               /*onChanged: (value){
                 titleInput = value;
               },*/
@@ -30,17 +43,14 @@ class NewTransaction extends StatelessWidget {
                 labelText: 'Amount',
               ),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData,
               //onChanged: (value) => amountInput = value,
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: (){
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
             ),
           ],
         ),
